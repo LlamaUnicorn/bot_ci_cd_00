24f1349a17e92c7942f81967e0efcb1ec7553faa
LlamaUnicorn <holidaynight@gmail.com>
2023-08-20 10:59:04 +0300

    setting up docker-compose

---
+++ script.sh
@@ -0,0 +1,5 @@
+cd /home/dave/PycharmProjects/bot_ci_cd_00
+git pull origin main
+docker-compose -f /home/dave/PycharmProjects/bot_ci_cd_00/docker-compose.yml down --remove-orphans
+docker-compose -f /home/dave/PycharmProjects/bot_ci_cd_00/docker-compose.yml up -d
+
